fa96e6563293ef3718b0e560b8ae432fc829262a
Hythem Sidky <sidky@outlook.com>
2017-04-20 15:37:27 -0400

    Re-added trust region condition.

--- src/LMB.cpp
+++ src/LMB.cpp
@@ -132,8 +132,8 @@ bool LMB::step()
                    std::abs(currerr - preverr) >= stop.minimalValueDifferences) &&
                   (stop.minimalValue == // Target performance reached? 
                    StoppingCriteria::defaultValue.minimalValue || 
-                   sse >= stop.minimalValue);// && 
-                  //(lambda <= lmax) && // Maximum trust region radius
+                   sse >= stop.minimalValue) && 
+                  (lambda <= lmax);// && // Maximum trust region radius
                   //(std::abs(gammap - gamma) >= stop.minimalSearchSpaceStep); 
 
   // Scale lambda lower since it did a good job
